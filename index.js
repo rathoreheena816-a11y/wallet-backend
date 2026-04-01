7969d2947011126cce8f6baa9653fcaa4abe77a3
Heena <rathoreheena816@gmail.com>
2026-04-01 17:42:26 +0530

    fixed port issue

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ app.get('/wallet/create', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT} 🚀);
+  console.log("Server running on port " + PORT);
 });
